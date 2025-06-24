d8f6107038df5bac6bc0b21e14e117b0bbf0192b
Johnny Wu <johnnyw8@uci.edu>
2025-06-24 03:31:32 +0000

    feat(app): add toolbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, MatToolbarModule, MatButtonModule, MatTooltipModule, MatIconModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected title = 'friend-tracker';
+
+  openAddContactDialog() {
+    console.log('opens add contact dialog')
+  }
 }
