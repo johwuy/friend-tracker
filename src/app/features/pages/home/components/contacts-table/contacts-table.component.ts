7e3911a3b35c4f93c27b3239d488b959fbcdc5e1
Johnny Wu <johnnyw8@uci.edu>
2025-06-27 06:12:26 +0000

    feat(contacts-table): scaffold contacts table

---
+++ src/app/features/pages/home/components/contacts-table/contacts-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contacts-table',
+  imports: [],
+  templateUrl: './contacts-table.component.html',
+  styleUrl: './contacts-table.component.scss'
+})
+export class ContactsTableComponent {
+
+}
