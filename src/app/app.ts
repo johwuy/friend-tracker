5b1b20ec8843d2b0b2e2b02f7d20b041938bd0c1
Johnny Wu <johnnyw8@uci.edu>
2025-07-07 00:57:38 +0000

    feat(toolbar): conditonally show add contact button

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { RouterLink } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatIconModule } from '@angular/material/icon';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -14,8 +15,15 @@ import { MatIconModule } from '@angular/material/icon';
 })
 export class App {
   protected title = 'friend-tracker';
+  private routerService = inject(Router);
+  protected isHomePath = false;
+
+  constructor() {
+    this.routerService.events.pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(event => this.isHomePath = event.url === '/home');
+  }
 
   openAddContactDialog() {
-    console.log('opens add contact dialog')
+    console.log('opens add contact dialog');
   }
 }
