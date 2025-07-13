b0144659763f3a49ba883487ed61784dc1a34461
Johnny Wu <johnnyw8@uci.edu>
2025-07-13 23:59:16 +0000

    feat(raw-contact): create raw contact type

--- src/app/shared/models/contact.ts
+++ src/app/shared/models/contact.ts
@@ -3,3 +3,7 @@ import { ContactFormData } from "./contact-form-data";
 export interface Contact extends ContactFormData {
   id: string;
 }
+
+export interface RawContact extends Omit<Contact, 'birthday'> {
+  birthday?: string;
+}
