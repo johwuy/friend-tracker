4d950920a3cb1cdf9af17ebe4cf9d9f3f6851ea7
Johnny Wu <johnnyw8@uci.edu>
2025-07-14 01:32:03 +0000

    feat(create-contact-dialog): add closing button in header

--- src/app/features/pages/home/components/create-contact/create-contact.dialog.ts
+++ src/app/features/pages/home/components/create-contact/create-contact.dialog.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-create-contact',
-  imports: [MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose, MatButtonModule],
+  imports: [MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose, MatButtonModule, MatIconModule],
   templateUrl: './create-contact.dialog.html',
   styleUrl: './create-contact.dialog.scss'
 })
