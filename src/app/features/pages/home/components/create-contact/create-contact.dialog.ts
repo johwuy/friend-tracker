aae43c11468b0270a42ae2327f95ec7ea68b29d4
Johnny Wu <johwuy@gmail.com>
2025-07-23 04:23:28 +0000

    feat(create-contact-dialog): create form group

--- src/app/features/pages/home/components/create-contact/create-contact.dialog.ts
+++ src/app/features/pages/home/components/create-contact/create-contact.dialog.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
+import { RelationshipStatus } from '@shared/constants/relationship-status';
 
 @Component({
   selector: 'app-create-contact',
-  imports: [MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose, MatButtonModule, MatIconModule],
+  imports: [MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose, MatButtonModule, MatIconModule, ReactiveFormsModule],
   templateUrl: './create-contact.dialog.html',
   styleUrl: './create-contact.dialog.scss'
 })
 export class CreateContactDialog {
-
+  contactForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    birthday: new FormControl<Date | null>(null),
+    email: new FormControl<string | null>(null, Validators.email),
+    phoneNumber: new FormControl<string | null>(null),
+    relationship: new FormControl<RelationshipStatus | null>(null)
+  });
 }
