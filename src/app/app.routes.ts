88e23528688b809d27a64ea6fbab72540943b3b1
Johnny Wu <johwuy@gmail.com>
2025-07-31 04:40:50 +0000

    feat(contact): create route to contact page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './features/pages/home/home.page';
+import { ContactPage } from '@features/pages/contact/contact.page';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomePage }
+  { path: 'home', component: HomePage },
+  { path: 'contact', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'contact/:id', component: ContactPage }
 ];
