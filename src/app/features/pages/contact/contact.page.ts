639046048aeae2b68bd29e2ccfcf2bbd4c6818c4
Johnny Wu <johwuy@gmail.com>
2025-07-31 05:12:50 +0000

    feat(contact-page): implement fetching contact

--- src/app/features/pages/contact/contact.page.ts
+++ src/app/features/pages/contact/contact.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ContactsService } from '@core/services/contacts.service';
+import { Contact } from '@shared/models/contact';
 
 @Component({
   selector: 'app-contact',
@@ -6,6 +9,21 @@ import { Component } from '@angular/core';
   templateUrl: './contact.page.html',
   styleUrl: './contact.page.scss'
 })
-export class ContactPage {
+export class ContactPage implements OnInit {
+  private activatedRoute = inject(ActivatedRoute);
+  private contactsService = inject(ContactsService);
+  protected data = signal<Contact | null>(null);
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      this.fetchContact(params['id']);
+    })
+  }
+
+  private fetchContact(id: string) {
+    this.contactsService.getContact(id).subscribe({
+      next: contact => this.data.set(contact)
+    });
+  }
 
 }
