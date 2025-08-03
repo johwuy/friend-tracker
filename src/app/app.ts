942b73db66f0bfe4ee1046f79cfd4210135bb0e7
Johnny Wu <johwuy@gmail.com>
2025-08-03 05:00:39 +0000

    feat(home): use toolbar service

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,10 @@
 import { Component, inject } from '@angular/core';
-import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
-import { RouterLink } from '@angular/router';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatIconModule } from '@angular/material/icon';
-import { filter } from 'rxjs/operators';
-import { MatDialog } from '@angular/material/dialog';
-import { CreateContactDialog } from '@features/pages/home/components/create-contact/create-contact.dialog';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { ToolbarSerivce } from '@core/services/toolbar.serivce';
 
 @Component({
   selector: 'app-root',
@@ -18,18 +15,9 @@ import { CreateContactDialog } from '@features/pages/home/components/create-cont
 export class App {
   protected title = 'friend-tracker';
 
-  private dialogService = inject(MatDialog);
-  private routerService = inject(Router);
-  protected isHomePath = false;
+  protected toolbarService = inject(ToolbarSerivce);
+  protected actions = this.toolbarService.toolbarActions;
 
-  constructor() {
-    this.routerService.events.pipe(filter(event => event instanceof NavigationEnd))
-      .subscribe(event => this.isHomePath = event.urlAfterRedirects.startsWith('/home'));
-  }
+  constructor() { }
 
-  openAddContactDialog() {
-    this.dialogService.open(CreateContactDialog, {
-      width: '50%'
-    });
-  }
 }
--- src/app/features/pages/home/home.page.ts
+++ src/app/features/pages/home/home.page.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ToolbarSerivce } from '@core/services/toolbar.serivce';
 import { ContactsTableComponent } from './components/contacts-table/contacts-table.component';
+import { CreateContactDialog } from './components/create-contact/create-contact.dialog';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,26 @@ import { ContactsTableComponent } from './components/contacts-table/contacts-tab
   templateUrl: './home.page.html',
   styleUrl: './home.page.scss'
 })
-export class HomePage {
+export class HomePage implements OnInit, OnDestroy {
+  private dialogService = inject(MatDialog);
+  private toolbarService = inject(ToolbarSerivce);
+
+  ngOnInit(): void {
+    this.toolbarService.setActions([{
+      icon: 'person_add',
+      tooltip: 'Add contact',
+      callback: this.openDialog
+    }]);
+  }
+
+  ngOnDestroy(): void {
+    this.toolbarService.clearActions();
+  }
+
+  private openDialog = () => {
+    this.dialogService.open(CreateContactDialog, {
+      width: '50%'
+    });
+  }
 
 }
