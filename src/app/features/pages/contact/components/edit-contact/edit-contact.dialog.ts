9028c721d23649646a612634235112a7c5964d2b
Johnny Wu <johwuy@gmail.com>
2025-08-04 05:17:48 +0000

    feat(edit-contact-dialog): add form

--- src/app/features/pages/contact/components/edit-contact/edit-contact.dialog.ts
+++ src/app/features/pages/contact/components/edit-contact/edit-contact.dialog.ts
@@ -1,20 +1,27 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { ContactsService } from '@core/services/contacts.service';
-import { RelationshipStatus } from '@shared/constants/relationship-status';
+import { RelationshipStatus, RelationshipStatuses } from '@shared/constants/relationship-status';
 import { DateTime } from 'luxon';
 
 @Component({
   selector: 'app-edit-contact',
-  imports: [MatDialogModule, MatButtonModule],
+  imports: [MatDialogModule, MatButtonModule, MatIconModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatSelectModule],
   templateUrl: './edit-contact.dialog.html',
   styleUrl: './edit-contact.dialog.scss'
 })
 export class EditContactDialog implements OnInit {
   protected contactsService = inject(ContactsService);
   readonly id = signal(inject<string>(MAT_DIALOG_DATA));
+
+  protected statuses = RelationshipStatuses;
 
   contactForm = new FormGroup({
     id: new FormControl(this.id(), {
@@ -37,4 +44,7 @@ export class EditContactDialog implements OnInit {
     });
   }
 
+  get birthday() {
+    return this.contactForm.get('birthday');
+  }
 }
