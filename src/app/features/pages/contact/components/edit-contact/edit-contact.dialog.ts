ff89d047fb708bcb65b6028ab761d0ff7e839293
Johnny Wu <johwuy@gmail.com>
2025-08-04 05:03:55 +0000

    feat(edit-contact-dialog): add form group

--- src/app/features/pages/contact/components/edit-contact/edit-contact.dialog.ts
+++ src/app/features/pages/contact/components/edit-contact/edit-contact.dialog.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+import { RelationshipStatus } from '@shared/constants/relationship-status';
+import { DateTime } from 'luxon';
 
 @Component({
   selector: 'app-edit-contact',
@@ -9,5 +12,19 @@ import { MatDialogModule } from '@angular/material/dialog';
   styleUrl: './edit-contact.dialog.scss'
 })
 export class EditContactDialog {
-
+  readonly id = signal(inject<string>(MAT_DIALOG_DATA));
+  contactForm = new FormGroup({
+    id: new FormControl(this.id(), {
+      validators: Validators.required,
+      nonNullable: true
+    }),
+    name: new FormControl('', {
+      validators: Validators.required,
+      nonNullable: true
+    }),
+    birthday: new FormControl<DateTime | null>(null),
+    email: new FormControl<string | null>(null, Validators.email),
+    phoneNumber: new FormControl<string | null>(null),
+    relationship: new FormControl<RelationshipStatus | null>(null)
+  });
 }
