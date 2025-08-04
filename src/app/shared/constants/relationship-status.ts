38c009b9fa9ee86fa7d6b557870dabe5a859dbb1
Johnny Wu <johwuy@gmail.com>
2025-08-04 05:38:07 +0000

    feat: convert string literal into array of objects w/ label and value

--- src/app/shared/constants/relationship-status.ts
+++ src/app/shared/constants/relationship-status.ts
@@ -1,2 +1,10 @@
-export const RelationshipStatuses = ["Family", "Friend", "Colleague", "Pet"] as const;
-export type RelationshipStatus = typeof RelationshipStatuses[number];
+export const RelationshipStatuses = [
+  {label: "Friend", value: "Friend"},
+  {label: "Family", value: "Family"},
+  {label: 'Colleague', value: "Colleague"},
+  {label: 'Pet', value: "Pet"},
+  {label: 'None', value: null}
+] as const;
+
+export type RelationshipStatus = (typeof RelationshipStatuses)[number]['value'];
+
