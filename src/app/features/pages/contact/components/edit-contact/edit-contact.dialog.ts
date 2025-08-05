d461b1a641528c8101923d4ba46496103b404c17
Johnny Wu <johwuy@gmail.com>
2025-08-05 06:35:48 +0000

    feat(edit-contact-dialog): show progress spinner when loading

--- src/app/features/pages/contact/components/edit-contact/edit-contact.dialog.ts
+++ src/app/features/pages/contact/components/edit-contact/edit-contact.dialog.ts
@@ -6,6 +6,7 @@ import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
 import { ContactsService } from '@core/services/contacts.service';
 import { RelationshipStatus, RelationshipStatuses } from '@shared/constants/relationship-status';
@@ -13,7 +14,7 @@ import { DateTime } from 'luxon';
 
 @Component({
   selector: 'app-edit-contact',
-  imports: [MatDialogModule, MatButtonModule, MatIconModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatSelectModule],
+  imports: [MatDialogModule, MatButtonModule, MatIconModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatSelectModule, MatProgressSpinnerModule],
   templateUrl: './edit-contact.dialog.html',
   styleUrl: './edit-contact.dialog.scss'
 })
@@ -21,6 +22,7 @@ export class EditContactDialog implements OnInit {
   protected readonly contactsService = inject(ContactsService);
   private readonly id = signal(inject<string>(MAT_DIALOG_DATA));
 
+  protected loading = false;
   protected readonly statuses = RelationshipStatuses;
 
   contactForm = new FormGroup({
@@ -39,8 +41,10 @@ export class EditContactDialog implements OnInit {
   });
 
   ngOnInit(): void {
+    this.loading = true;
     this.contactsService.getContact(this.id()).subscribe(contact => {
       this.contactForm.setValue(contact);
+      this.loading = false;
     });
   }
 
