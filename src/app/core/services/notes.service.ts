fa237cc43153d8d5b078baa0408343caaf80ab84
Johnny Wu <johwuy@gmail.com>
2025-08-10 06:27:27 +0000

    feat(notes-service): add get note

--- src/app/core/services/notes.service.ts
+++ src/app/core/services/notes.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Note } from '@shared/models/note';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotesService {
+  private readonly httpService = inject(HttpClient);
+  private readonly API_URL = 'http://localhost:5149/api/notes';
 
-  constructor() { }
+  getNote(id: string) {
+    return this.httpService.get<Note>(`${this.API_URL}/${id}`);
+  }
 }
