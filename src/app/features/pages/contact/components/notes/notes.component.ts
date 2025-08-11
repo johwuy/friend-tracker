b848580e0a4364dffc1d4f0ce47cf91cda583b7f
Johnny Wu <johwuy@gmail.com>
2025-08-11 02:28:50 +0000

    feat(notes): create form control and fetching

--- src/app/features/pages/contact/components/notes/notes.component.ts
+++ src/app/features/pages/contact/components/notes/notes.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, input, OnInit } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { NotesService } from '@core/services/notes.service';
 
 @Component({
   selector: 'app-notes',
-  imports: [],
+  imports: [ReactiveFormsModule, MatInputModule],
   templateUrl: './notes.component.html',
   styleUrl: './notes.component.scss'
 })
-export class NotesComponent {
+export class NotesComponent implements OnInit {
+  private readonly notesService = inject(NotesService);
   readonly contactId = input.required<string>();
+  notes = new FormControl<string | null>(null);
 
+  protected readonly placeholderText = "Add notes about family members, parents' names or anything else you want to remember...";
+
+  ngOnInit(): void {
+    this.notesService.getNote(this.contactId()).subscribe( note => {
+      this.notes.setValue(note.content);
+    });
+  }
 }
