22bde44bb2fc3cdcb0e107ad99cf62c0620a3b05
Johnny Wu <johwuy@gmail.com>
2025-08-11 05:22:48 +0000

    feat(interactions): add fetching of interaction

--- src/app/features/pages/contact/components/interactions/interactions.component.ts
+++ src/app/features/pages/contact/components/interactions/interactions.component.ts
@@ -1,7 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, input, OnInit, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog } from '@angular/material/dialog';
 import { InteractionDialog } from './interaction/interaction.dialog';
+import { Interaction } from '@shared/models/interaction';
+import { InteractionsService } from '@core/services/interactions.service';
 
 @Component({
   selector: 'app-interactions',
@@ -9,8 +11,20 @@ import { InteractionDialog } from './interaction/interaction.dialog';
   templateUrl: './interactions.component.html',
   styleUrl: './interactions.component.scss'
 })
-export class InteractionsComponent {
+export class InteractionsComponent implements OnInit {
   private readonly dialogService = inject(MatDialog);
+  private readonly interactionsService = inject(InteractionsService);
+
+  readonly contactId = input.required<string>();
+
+  private readonly _interactionsSignal = signal<Interaction[]>([]);
+  readonly interactions = this._interactionsSignal.asReadonly();
+
+  ngOnInit(): void {
+    this.interactionsService.fectchInteractions(this.contactId()).subscribe(result => {
+      this._interactionsSignal.set(result);
+    })
+  }
 
   protected handleDialog() {
     this.dialogService.open(InteractionDialog, {
