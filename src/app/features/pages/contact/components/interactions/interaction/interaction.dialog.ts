f0ee96692a7b99edfbeb86434a4c07a3a3114a1b
Johnny Wu <johwuy@gmail.com>
2025-08-11 05:32:54 +0000

    feat(interaction-dialog): add close button

--- src/app/features/pages/contact/components/interactions/interaction/interaction.dialog.ts
+++ src/app/features/pages/contact/components/interactions/interaction/interaction.dialog.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 import { DateTime } from 'luxon';
 
 @Component({
   selector: 'app-interaction',
-  imports: [MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose, MatButtonModule, ReactiveFormsModule],
+  imports: [MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose, MatButtonModule, ReactiveFormsModule, MatIconModule],
   templateUrl: './interaction.dialog.html',
   styleUrl: './interaction.dialog.scss'
 })
