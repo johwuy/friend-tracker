6939414c1489c27a1536a3a8857c2ef2d5e202be
Johnny Wu <johwuy@gmail.com>
2025-08-11 05:27:31 +0000

    feat(interaction-dialog): add form group and form control getter

--- src/app/features/pages/contact/components/interactions/interaction/interaction.dialog.ts
+++ src/app/features/pages/contact/components/interactions/interaction/interaction.dialog.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle } from '@angular/material/dialog';
+import { DateTime } from 'luxon';
 
 @Component({
   selector: 'app-interaction',
-  imports: [MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose, MatButtonModule],
+  imports: [MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose, MatButtonModule, ReactiveFormsModule],
   templateUrl: './interaction.dialog.html',
   styleUrl: './interaction.dialog.scss'
 })
 export class InteractionDialog {
+  protected readonly interactionForm = new FormGroup({
+    date: new FormControl<DateTime>(DateTime.local()),
+    content: new FormControl<string>('')
+  });
 
+  get dateControl() {
+    return this.interactionForm.get('date');
+  }
+
+  get contentControl() {
+    return this.interactionForm.get('content');
+  }
 }
