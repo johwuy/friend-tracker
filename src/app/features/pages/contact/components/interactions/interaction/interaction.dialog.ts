1451817ed9f603cf4e5949789b98b152f3ca7af5
Johnny Wu <johwuy@gmail.com>
2025-08-11 07:37:35 +0000

    feat(interaction-dialog): add getting raw value

--- src/app/features/pages/contact/components/interactions/interaction/interaction.dialog.ts
+++ src/app/features/pages/contact/components/interactions/interaction/interaction.dialog.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, inject, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { Interaction, InteractionDTO } from '@shared/models/interaction';
 import { DateTime } from 'luxon';
 
 @Component({
@@ -14,10 +15,20 @@ import { DateTime } from 'luxon';
   styleUrl: './interaction.dialog.scss'
 })
 export class InteractionDialog {
+  private readonly id = signal(inject<string>(MAT_DIALOG_DATA));
   protected readonly interactionForm = new FormGroup({
-    date: new FormControl<string>(DateTime.now().toFormat('yyyy-MM-dd')),
-    content: new FormControl<string>('')
+    date: new FormControl<string>(DateTime.now().toFormat('yyyy-MM-dd'), { nonNullable: true, validators: Validators.required }),
+    content: new FormControl<string>('', { nonNullable: true, validators: Validators.required })
   });
+
+  get rawValue(): Interaction | InteractionDTO {
+    const parsedDate = DateTime.fromFormat(this.dateControl!.value, 'yyyy-MM-dd');
+    return {
+      ...this.interactionForm.getRawValue(),
+      date: parsedDate,
+      ...(this.id() && {id: this.id()})
+    };
+  }
 
   get dateControl() {
     return this.interactionForm.get('date');
