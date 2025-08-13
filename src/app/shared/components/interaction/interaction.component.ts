0749fc4df2bbb58d08fe160643f151bedcd017c6
Johnny Wu <johwuy@gmail.com>
2025-08-13 04:45:21 +0000

    feat(interaction): scaffold component

---
+++ src/app/shared/components/interaction/interaction.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interaction',
+  imports: [],
+  templateUrl: './interaction.component.html',
+  styleUrl: './interaction.component.scss'
+})
+export class InteractionComponent {
+
+}
