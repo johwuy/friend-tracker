613df44376cdd121de41c90f73a9dc7457fb0efc
Johnny Wu <johwuy@gmail.com>
2025-08-13 06:07:05 +0000

    feat(interaction-dialog): add ssuport for returning interaction and interactionDto

--- src/app/features/pages/contact/components/interactions/interaction/interaction.dialog.ts
+++ src/app/features/pages/contact/components/interactions/interaction/interaction.dialog.ts
@@ -5,7 +5,7 @@ import { MAT_DIALOG_DATA, MatDialogActions, MatDialogClose, MatDialogContent, Ma
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { Interaction, InteractionDTO } from '@shared/models/interaction';
+import { CreateInteractionDialogData, Interaction, InteractionDTO } from '@shared/models/interaction';
 import { DateTime } from 'luxon';
 
 @Component({
@@ -15,19 +15,21 @@ import { DateTime } from 'luxon';
   styleUrl: './interaction.dialog.scss'
 })
 export class InteractionDialog {
-  protected readonly id = signal(inject<string>(MAT_DIALOG_DATA));
+  protected readonly data = signal(inject<CreateInteractionDialogData | Interaction>(MAT_DIALOG_DATA));
   protected readonly interactionForm = new FormGroup({
     date: new FormControl<string>(DateTime.now().toFormat('yyyy-MM-dd'), { nonNullable: true, validators: Validators.required }),
     content: new FormControl<string>('', { nonNullable: true, validators: Validators.required })
   });
 
   get rawValue(): Interaction | InteractionDTO {
-    const parsedDate = DateTime.fromFormat(this.dateControl!.value, 'yyyy-MM-dd');
-    return {
+    const existingData = this.data();
+    const data = {
       ...this.interactionForm.getRawValue(),
-      date: parsedDate,
-      ...(this.id() && {id: this.id()})
+      date: DateTime.fromFormat(this.dateControl!.value, 'yyyy-MM-dd'),
+      contactId: this.data().contactId,
     };
+
+    return ('id' in existingData) ? {...data, id: existingData.id} : data;
   }
 
   get dateControl() {
