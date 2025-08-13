cea7e2e19f19b63cd9adeda09faea47e2ea34361
Johnny Wu <johwuy@gmail.com>
2025-08-13 06:36:57 +0000

    feat: add ability to update interaction

--- src/app/shared/components/interaction/interaction.component.ts
+++ src/app/shared/components/interaction/interaction.component.ts
@@ -1,7 +1,9 @@
-import { Component, input, output } from '@angular/core';
+import { Component, inject, input, output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatDialog } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
+import { InteractionDialog } from '@features/pages/contact/components/interactions/interaction/interaction.dialog';
 import { Interaction } from '@shared/models/interaction';
 
 @Component({
@@ -11,10 +13,23 @@ import { Interaction } from '@shared/models/interaction';
   styleUrl: './interaction.component.scss'
 })
 export class InteractionComponent {
+  private readonly dialogService = inject(MatDialog);
   readonly interaction = input.required<Interaction>();
   readonly deleteInteractionClick = output<string>();
+  readonly updateInteractionEvent = output<Interaction>();
 
   deleteInteraction() {
     this.deleteInteractionClick.emit(this.interaction().id);
+  }
+
+  updateInteraction() {
+    const diagRef = this.dialogService.open<InteractionDialog, Interaction>(InteractionDialog, {
+      width: '50%',
+      data: this.interaction()
+    });
+
+    diagRef.afterClosed().subscribe((result: Interaction) => {
+      this.updateInteractionEvent.emit(result);
+    })
   }
 }
